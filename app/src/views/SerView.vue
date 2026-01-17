76b3a9b9567cdc453288b6ae3660f87bfe7f7bd7
Ian Ndegwa <ndegwaian241@gmail.com>
2026-01-17 20:16:50 +0300

    message

--- app/src/views/SerView.vue
+++ app/src/views/SerView.vue
@@ -8,8 +8,8 @@
 
 <script setup>
 import serhero from "../components/services/serhero.vue";
-import solutions from "../components/services/Solutions.vue";
-import ready from "../components/services/Ready.vue";
+import solutions from "../components/services/solutions.vue";
+import ready from "../components/services/ready.vue";
 </script>
 
 <style lang="scss" scoped></style>
